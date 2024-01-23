efe5700a85a2cbd7220df1633cbb5fdc5dcdb1f5
sebastien <sebastien.pingal@gmail.com>
2024-01-23 18:03:43 +0100

    Update page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import PerlinBackground from '@/components/perlinBackground.jsx';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center h-full bg-opacity-0'>
+    <div className='flex flex-col items-center h-full gap-5'>
       <div style={{ overflow: 'hidden', borderRadius: '50%', height: '370px' }}>
         <Image src='/me.jpeg' width={400} height={400} className='rounded-full' alt='Sébastien' />
       </div>
-      <p className='text-lg'>
+      <p className='text-lg backdrop-blur-xl shadow-inner p-2 rounded bg-card:10'>
         Hey, I'am Sébastien. I am designer, software tinker, music maker, and sometimes a teacher.
         I am curently working on this website, so it's a bit empty for now. But I'm glad to finaly making somekind of hub for all my projects.
       </p>
