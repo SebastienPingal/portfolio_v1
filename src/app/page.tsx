525c90185d84138da5c318608c10b7193e42c29d
sebastien <sebastien.pingal@gmail.com>
2024-01-23 15:28:21 +0100

    Update(home): added some content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className='flex flex-col items-center h-full'>
+      <p className='text-lg'>
+        Hey, I'am Sébastien. I am designer, software tinker, music maker, and sometimes a teacher.
+        I am curently working on this website, so it's a bit empty for now. But I'm glad to finaly making somekind of hub for all my projects.
+      </p>
+
     </div>
   );
 }
