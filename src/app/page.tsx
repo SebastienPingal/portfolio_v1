4e5183250dd357defca121d0d907824d1079871c
sebastien <sebastien.pingal@gmail.com>
2024-01-24 14:15:59 +0100

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Image src='/me.jpeg' width={400} height={400} className='rounded-full' alt='Sébastien' />
       </div>
       <p className='text-lg backdrop-blur-xl shadow-inner p-2 rounded'>
-        Hey, I&apos;m Sébastien. I am designer, software tinker, music maker, and sometimes a teacher.
+        Hey, I&apos;m Sébastien. I am designer, software tinkerer, music maker, and sometimes a teacher.
         I am curently working on this website, so it&apos;s a bit empty for now. But I&apos;m glad to finaly making somekind of hub for all my projects.
       </p>
     </div>
