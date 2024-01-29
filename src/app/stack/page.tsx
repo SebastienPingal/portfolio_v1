3e58d711895fe7416e5442b13615c9903fe9367c
sebastien <sebastien.pingal@gmail.com>
2024-01-29 17:50:35 +0100

    clean

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -14,26 +14,26 @@ const StackPage = async () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
         {techStack.map((tech, index) => {
           return (
-          <Card key={index} className='w-full'>
-            <CardHeader>
-              <CardTitle className='flex gap-3 items-center'>
-                <div className='bg-white/90 w-16 h-16 rounded relative'>
-                  <Image src={tech.logo} alt={tech.title} fill={true} className='object-contain p-1' />
-                </div>
-                  <h2>{tech.title}</h2>
-
-              </CardTitle>
-            </CardHeader>
-            <CardContent className='flex flex-col gap-2'>
-              <CardDescription>{tech.description}</CardDescription>
-              <a href={tech.link} target="_blank" rel="noreferrer">
-                <Button className='flex gap-2'>
-                  Learn More <MoveUpRight className="w-4 h-4" />
-                </Button>
-              </a>
-            </CardContent>
-          </Card>
-        ) })}
+            <Card key={index} className='w-full'>
+              <CardHeader>
+                <CardTitle className='flex gap-3 items-center'>
+                  <div className='bg-white/90 w-16 h-16 rounded relative border-2 border-primary/60'>
+                    <Image src={tech.logo} alt={tech.title} fill={true} className='object-contain p-1' />
+                  </div>
+                  <p className='text-2xl'>{tech.title}</p>
+                </CardTitle>
+              </CardHeader>
+              <CardContent className='flex flex-col gap-2'>
+                <CardDescription>{tech.description}</CardDescription>
+                <a href={tech.link} target="_blank" rel="noreferrer">
+                  <Button className='flex gap-2'>
+                    Learn More <MoveUpRight className="w-4 h-4" />
+                  </Button>
+                </a>
+              </CardContent>
+            </Card>
+          )
+        })}
       </div>
     </div>
   )
