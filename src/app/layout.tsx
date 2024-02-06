f58d23a66491ac59409f6fbe07c43cc011670a85
sebastien <sebastien.pingal@gmail.com>
2024-02-06 13:38:12 +0100

    feat(NavBar): responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,20 @@ import SessionProvider from "./SessionProvider"
 import { ThemeProvider } from "./theme-provider"
 
 import Navbar from "@/components/NavBar"
+import { Button } from "@/components/ui/button"
 import PerlinBackground from "@/components/perlinBackground.jsx"
 import { Toaster } from "@/components/ui/toaster"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 import { getPosts } from "./actions"
+import { MenuIcon } from "lucide-react"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +35,7 @@ export default async function RootLayout({
   const blogPosts = await getPosts("BLOGPOST")
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={[cereal.className, "flex w-screen h-screen overflow-hidden"].join(" ")}>
+      <body className={[cereal.className, "flex flex-col sm:flex-row w-screen h-screen overflow-hidden"].join(" ")}>
         <Toaster />
         <ThemeProvider
           attribute="class"
@@ -33,8 +43,20 @@ export default async function RootLayout({
           enableSystem
         >
           <SessionProvider>
-            <Navbar workPosts={workPosts} blogPosts={blogPosts} />
-            <main className="flex-1 page z-10 max-w-4xl mx-auto p-8 overflow-auto">{children}</main>
+            <div className="sm:hidden z-20 w-full fixed backdrop-blur-md">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="h-10 w-full">
+                  <Button className="w-full rounded-none">
+                    <MenuIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent sideOffset={0}>
+                  <Navbar workPosts={workPosts} blogPosts={blogPosts} className="w-screen" />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <Navbar workPosts={workPosts} blogPosts={blogPosts} className="hidden sm:flex" />
+            <main className="flex-1 page z-10 max-w-4xl mx-auto p-8 overflow-auto pt-16 sm:pt-8">{children}</main>
             <PerlinBackground />
           </SessionProvider>
         </ThemeProvider>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,7 +30,7 @@ const CerberesBlack = '/cerberes_black.svg'
 import { Post } from "@prisma/client"
 import PostSession from "./PostSession"
 
-const NavBar = ({ workPosts, blogPosts }: { workPosts: Post[], blogPosts: Post[] }) => {
+const NavBar = ({ workPosts, blogPosts, className }: { workPosts: Post[], blogPosts: Post[], className?: string }) => {
   const session = useSession()
   const pathname = usePathname()
   const { theme } = useTheme()
@@ -41,103 +41,101 @@ const NavBar = ({ workPosts, blogPosts }: { workPosts: Post[], blogPosts: Post[]
   }, [theme])
 
   return (
-    <>
-      <NavigationMenu orientation="vertical" className='items-start shadow-inner h-full bg-card:10  bg-card/20 backdrop-blur-xl'>
-        <NavigationMenuList className='flex flex-col gap-1 items-start w-56 p-2'>
-          <div className='text-2xl font-extrabold'>Sébastien Pingal</div>
-          <div className="w-full">
-            <NavigationMenuItem className={pathname === '/' ? 'bg-popover text-popover-foreground' : ''}>
-              <Link href="/" className="w-full" legacyBehavior passHref>
-                <NavigationMenuLink className="w-full">
-                  <Home className="w-4 h-4" />Home
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </div>
+    <NavigationMenu orientation="vertical" className={className + ' items-start shadow-inner h-full bg-card:10  bg-card/20 backdrop-blur-xl'}>
+      <NavigationMenuList className='flex flex-col gap-1 items-start p-2 w-screen sm:w-56'>
+        <div className='text-2xl font-extrabold w-full'>Sébastien Pingal</div>
+        <div className="w-full">
+          <NavigationMenuItem className={pathname === '/' ? 'bg-popover text-popover-foreground' : ''}>
+            <Link href="/" className="w-full" legacyBehavior passHref>
+              <NavigationMenuLink className="w-full">
+                <Home className="w-4 h-4" />Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </div>
 
-          <div className='w-full'>
-            <NavigationMenuItem className="font-extrabold mb-2">
-              Me
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/stack" className="w-full" legacyBehavior passHref>
-                <NavigationMenuLink className="w-full">
-                  <PencilRuler className="w-4 h-4" /> Stack
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </div>
+        <div className='w-full'>
+          <NavigationMenuItem className="font-extrabold mb-2">
+            Me
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/stack" className="w-full" legacyBehavior passHref>
+              <NavigationMenuLink className="w-full">
+                <PencilRuler className="w-4 h-4" /> Stack
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </div>
 
-          <div className='w-full'>
-            <NavigationMenuItem className="font-extrabold mb-2">
-              Projects
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://aestima-immo.com" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Image src={darkMode ? AestimaWhite : AestimaBlack} alt="Aestima-immo" width={16} height={16} />Aestima-immo<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://kafo.work" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Image src={darkMode ? KafoWhite : KafoBlack} alt="Kafo" width={16} height={16} />Kafo<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://cerberes.flamberger.zip" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Image src={darkMode ? CerberesWhite : CerberesBlack} alt="cerberes" width={16} height={16} />Cerberes<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-          </div>
+        <div className='w-full'>
+          <NavigationMenuItem className="font-extrabold mb-2">
+            Projects
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://aestima-immo.com" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Image src={darkMode ? AestimaWhite : AestimaBlack} alt="Aestima-immo" width={16} height={16} />Aestima-immo<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://kafo.work" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Image src={darkMode ? KafoWhite : KafoBlack} alt="Kafo" width={16} height={16} />Kafo<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://cerberes.flamberger.zip" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Image src={darkMode ? CerberesWhite : CerberesBlack} alt="cerberes" width={16} height={16} />Cerberes<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+        </div>
 
-          {(workPosts.length > 0 || session.data?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL) && (
-            <PostSession posts={workPosts} type="WORK" subSlug="work" title="Works" />
-          )}
+        {(workPosts.length > 0 || session.data?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL) && (
+          <PostSession posts={workPosts} type="WORK" subSlug="work" title="Works" />
+        )}
 
-          {(blogPosts.length > 0 || session.data?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL) && (
-            <PostSession posts={blogPosts} type="BLOGPOST" subSlug="blog" title="Writing" />
-          )}
+        {(blogPosts.length > 0 || session.data?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL) && (
+          <PostSession posts={blogPosts} type="BLOGPOST" subSlug="blog" title="Writing" />
+        )}
 
-          <div className='w-full'>
-            <NavigationMenuItem className="font-extrabold mb-2">
-              Online
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://www.linkedin.com/in/s%C3%A9bastien-pingal-582795b3/" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Linkedin fill={darkMode ? 'white' : 'black'} className="w-4 h-4" />Linkedin<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://github.com/SebastienPingal" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Github className="w-4 h-4" />GitHub<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <a href="https://gitlab.com/Brume_" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
-                <Gitlab className="w-4 h-4" />Gitlab<MoveUpRight className="absolute right-0 w-4 h-4" />
-              </a>
-            </NavigationMenuItem>
-          </div>
+        <div className='w-full'>
+          <NavigationMenuItem className="font-extrabold mb-2">
+            Online
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://www.linkedin.com/in/s%C3%A9bastien-pingal-582795b3/" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Linkedin fill={darkMode ? 'white' : 'black'} className="w-4 h-4" />Linkedin<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://github.com/SebastienPingal" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Github className="w-4 h-4" />GitHub<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="https://gitlab.com/Brume_" target="_blank" rel="noreferrer" className="flex gap-4 items-center relative w-full">
+              <Gitlab className="w-4 h-4" />Gitlab<MoveUpRight className="absolute right-0 w-4 h-4" />
+            </a>
+          </NavigationMenuItem>
+        </div>
 
-          <div className='w-full'>
-            <NavigationMenuItem hover={false}>
-              <ThemeToggler />
-            </NavigationMenuItem>
-            <NavigationMenuItem hover={false}>
-              {session.data ? (
-                <div className="flex flex-col gap-2 justify-between items-center w-full">
-                  <Image src={session.data.user?.image || ''} alt="profile" width={64} height={64} className="rounded-full" />
-                  <p>{session.data.user?.name}</p>
-                  <SignoutButton className="w-full" />
-                </div>
-              ) : (
-                <SigninButton className="w-full" />
-              )
-              }
-            </NavigationMenuItem>
-          </div>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </>
+        <div className='w-full'>
+          <NavigationMenuItem hover={false}>
+            <ThemeToggler />
+          </NavigationMenuItem>
+          <NavigationMenuItem hover={false}>
+            {session.data ? (
+              <div className="flex flex-col gap-2 justify-between items-center w-full">
+                <Image src={session.data.user?.image || ''} alt="profile" width={64} height={64} className="rounded-full" />
+                <p>{session.data.user?.name}</p>
+                <SignoutButton className="w-full" />
+              </div>
+            ) : (
+              <SigninButton className="w-full" />
+            )
+            }
+          </NavigationMenuItem>
+        </div>
+      </NavigationMenuList>
+    </NavigationMenu>
   )
 }
 
