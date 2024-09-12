8c22da0ca18c1ca987a303571c407d746fab0c9c
sebastien <sebastien.pingal@gmail.com>
2024-09-12 12:01:35 +0200

    styled the pdf

--- src/app/CV/CV.tsx
+++ src/app/CV/CV.tsx
@@ -1,93 +1,116 @@
-import React from 'react';
-import { CVProps } from '../../types/CV'
+'use client'
+
 import { Progress } from '@/components/ui/progress'
+import React from 'react';
+import { CVProps } from '../../types/CV';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
 
 const CV: React.FC<CVProps> = ({ data }) => {
 
   const Section = ({ title, children }: { title: string, children: React.ReactNode }) => (
     <section className="flex flex-col gap-2 glassPanel">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       {children}
     </section>
   );
 
+  const { theme } = useTheme()
+  const me = theme === 'light' ? '/img/me_black.svg' : '/img/me_white.svg'
+
+  const handlePrint = () => {
+    window.print()
+  }
+
   return (
-    <div className="glassPanel flex flex-col gap-4">
-      <header className='flex flex-col ml-8'>
-        <h1>{data.name}</h1>
-        <p>{data.title}</p>
-      </header>
+    <>
+      <Button onClick={handlePrint}>
+        Convert to pdf
+      </Button>
+      <div className="glassPanel flex flex-col gap-4">
 
-      <Section title="À Propos">
-        <p>{data.about}</p>
-      </Section>
+        <header className='flex gap-4 items-center ml-8'>
+          <Image src={me} alt="Me" width={50} height={50} />
+          <div>
+            <h1>{data.name}</h1>
+            <p>{data.title}</p>
+          </div>
+        </header>
 
-      <div className='flex gap-4'>
-        <div className='flex flex-col gap-4'>
-          <Section title="Contact">
-            <p>
-              GitHub: <a href={`https://${data.contact.github}`}>{data.contact.github}</a><br />
-              Email: {data.contact.email}<br />
-              Téléphone: {data.contact.phone}<br />
-              Localisation: {data.contact.location}
-            </p>
-          </Section>
+        <Section title="À Propos">
+          <p className='text-sm text-justify'>{data.about}</p>
+        </Section>
 
-          <Section title="Langues">
-            <ul>
-              {data.languages.map((lang, index) => (
-                <li key={index}>{lang.name}: {lang.level}</li>
-              ))}
-            </ul>
-          </Section>
-        </div>
 
-        <div className='flex flex-col gap-4'>
-          <Section title="Expérience Professionnelle">
-            {data.experience.map((exp, index) => (
-              <article key={index} className='glassPanel'>
-                <h3>{exp.title} - {exp.company}</h3>
-                <div className="pl-4">
+        <div className='flex gap-4'>
+          <div className='flex flex-col gap-4 w-1/4'>
+            <Section title="Contact">
+              <p className='text-sm text-justify'>
+                <a href={`https://${data.contact.github}`}>{data.contact.github}</a><br />
+                <a href={`mailto:${data.contact.email}`}>{data.contact.email}</a><br />
+                <a href={`tel:${data.contact.phone}`}>{data.contact.phone}</a><br />
+                <a href={`https://maps.google.com/?q=${data.contact.location}`}>{data.contact.location}</a>
+              </p>
+            </Section>
+
+            <Section title="Langues">
+              <div className='text-sm text-justify'>
+                {data.languages.map((lang, index) => (
+                  <p key={index}>{lang.name}: {lang.level}</p>
+                ))}
+              </div>
+            </Section>
+
+            <Section title="Technologies">
+              <div className="flex flex-col gap-1">
+                {data.skills.stack.map((skill, index) => (
+                  <div key={index} className='flex flex-col gap-1'>
+                    <span className="whitespace-nowrap text-sm">{skill.name}</span>
+                    <Progress value={skill.rating * 20} className="w-full h-1" />
+                  </div>
+                ))}
+              </div>
+            </Section>
+
+            <Section title="Autres Compétences">
+              <div className="flex flex-col gap-1">
+                {data.skills.other.map((skill, index) => (
+                  <div key={index} className='flex flex-col gap-1'>
+                    <span className="whitespace-nowrap text-sm">{skill.name}</span>
+                    <Progress value={skill.rating * 20} className="w-full h-1" />
+                  </div>
+                ))}
+              </div>
+            </Section>
+
+          </div>
+
+          <div className='flex flex-col gap-4'>
+            <Section title="Expérience Professionnelle">
+              {data.experience.map((exp, index) => (
+                <article key={index} className='glassPanel'>
+                  <h4 className='text-md'>{exp.title} - {exp.company}</h4>
                   <p className='text-sm uppercase'>{exp.period}</p>
-                  <ul className='text-md'>
-                    {exp.responsibilities.map((resp, respIndex) => (
-                      <li key={respIndex}>{resp}</li>
-                    ))}
-                  </ul>
-                </div>
-              </article>
-            ))}
-          </Section>
-
-          <Section title="Technologies">
-            <div className="grid grid-cols-3 gap-x-2 gap-y-1 items-center list-none">
-              {data.skills.stack.map((skill, index) => (
-                <div key={index}>
-                  <span className="whitespace-nowrap">{skill.name}</span>
-                  <Progress value={skill.rating * 20} className="w-full h-4" />
-                </div>
+                  <div className="pl-4">
+                    <div className='text-sm'>
+                      {exp.responsibilities.map((resp, respIndex) => (
+                        <p key={respIndex}>{resp}</p>
+                      ))}
+                    </div>
+                  </div>
+                </article>
               ))}
-            </div>
-          </Section>
+            </Section>
 
-          <Section title="Autres Compétences">
-            <div className="grid grid-cols-3 gap-x-2 gap-y-1 items-center list-none">
-              {data.skills.other.map((skill, index) => (
-                <div key={index}>
-                  <span className="whitespace-nowrap">{skill.name}</span>
-                  <Progress value={skill.rating * 20} className="w-full h-4" />
-                </div>
-              ))}
-            </div>
-          </Section>
+            <Section title="Formation">
+              <p className='text-sm text-justify'>{data.formation}</p>
+            </Section>
 
-          <Section title="Formation">
-            <p>{data.formation}</p>
-          </Section>
-
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
