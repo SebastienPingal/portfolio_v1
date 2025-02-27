5544a8922fd0aa9ad0902da5ef073149b273e5dd
Sebastien <sebastien.pingal@gmail.com>
2025-02-27 10:03:12 +0100

    fix(UI): Adjust layout overflow and width for responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
       <GoogleTagManager gtmId="GTM-P42CZR92" />
       <body className={[
         cereal.className, 
-        "flex flex-col sm:flex-row w-screen h-screen relative",
+        "flex flex-col sm:flex-row sm:w-screen h-screen relative w-[100vw] overflow-x-hidden sm:overflow-x-visible",
         "[&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']"
       ].join(" ")}>
         {/* Google Tag Manager (noscript) */}
